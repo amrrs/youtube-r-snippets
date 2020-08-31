306808f9f53948d64a68a64f5e36d58b27fb941f
amrrs <1littlecoder@gmail.com>
2020-08-31 15:50:26 +0530

    Create matrix.sh

---
+++ matrix.sh
@@ -0,0 +1,13 @@
+function matrix () {
+      echo -e "\e[1;40m"
+      clear
+      while :; do
+          echo $LINES $COLUMNS $(( $RANDOM % $COLUMNS)) $(( $RANDOM % 72 ))
+          sleep 0.05
+      done|awk '{
+          letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん@#$%^&*()";
+          c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter;
+          printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }
+      }'
+  }
+reply
