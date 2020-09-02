fd1b8f9f2d1eb4a0d9d88e777bc24e768ff4fd4d
amrrs <1littlecoder@gmail.com>
2020-09-02 22:52:52 +0530

    Update covid-gtrends-diff-geo.R

--- covid-gtrends-diff-geo.R
+++ covid-gtrends-diff-geo.R
@@ -2,7 +2,9 @@ library(gtrendsR)
 library(tidyverse)
 
 
-res <- gtrends(c("covid"), geo = c('US','AR-C','IN','GB'))
+res <- gtrends(c("covid"), geo = c('US','AR-C','IN','GB')) #this downloads the "covid" search rankings for different regions defined by these country codes "US", "AR-C", "IN", "GB"
+
+# since the above object `res` is a list, we're trying to extract our relevant dataframe 
 
 resp <- res$interest_over_time
 
@@ -12,3 +14,7 @@ resp %>%
                            y = as.numeric(hits),
                            color = geo)) +
   theme_minimal()
+
+# saving the csv file in the current working directory
+
+write.csv(resp,"results.csv",row.names = F)
